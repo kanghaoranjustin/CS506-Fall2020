a0eda1b1ffeefa4dbd4b3ebc9caf0c3b2fb7c053
Haoran Kang <hrjkang@bu.edu>
2020-09-11 03:02:47 +0800

    hospital modified -v4

--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -1,3 +1,8 @@
-def draw_hospital():
-    print("hospital not found")
+def draw_hospital(height=10, width=20):
+    for i in range(height//2):
+    	print(" "*(width//2)+"|")
+    print("-"*(width//2) + "+" + "-"*(width//2))
+    for i in range(height//2):
+    	print(" "*(width//2)+"|")
     return
+
